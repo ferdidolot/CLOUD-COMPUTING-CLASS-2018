a9279bddc295b5a8cf28aa9d2e08d9172ed70e08
raisauku <raisauku@gmail.com>
2018-03-02 01:17:36 +0100

    first commit

---
+++ Lab1/Lab1.guessnumber.py
@@ -0,0 +1,22 @@
+
+import random
+
+
+def guessnumber():
+    # To gererate the random number from 1 to 20
+    randomNumber = random.randrange(0, 20)
+
+    guessedByUser = False
+    userNr = int(input("Please, try to guess the number: "))
+
+    while guessedByUser == False:
+
+        if userNr == randomNumber:
+            guessedByUser = True
+            print("Yes! The number is {}. You found it!".format(randomNumber))
+        elif userNr > randomNumber:
+            userNr = int(input("The number is too high. Try another number: "))
+        else:
+            userNr = int(input("The number is too low. Try another number: "))
+
+guessnumber()
