4a4921b0e25cca5a94ccb50ffab16d78191c072f
ferdidolot <ferdi.dolot@gmail.com>
2018-03-07 12:26:54 -0800

    Print number of words

--- Lab2/WordCountTensorFlow_1.py
+++ Lab2/WordCountTensorFlow_1.py
@@ -5,12 +5,12 @@ import re
 from collections import Counter
 
 def get_tokens():
-   with open('FirstContactWithTensorFlow.txt', 'r') as tf:
-    text = tf.read()
+    with open('FirstContactWithTensorFlow.txt', 'r') as tf:
+        text = tf.read()
     tokens = nltk.word_tokenize(text)
     return tokens
 
 tokens = get_tokens()
 count = Counter(tokens)
 print (count.most_common(10))
-print(count)
+print(len(count))
