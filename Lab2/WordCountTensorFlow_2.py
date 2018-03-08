a5b1d1130c7b6544e56dd274e6b390dca4bdf2d8
raisauku <raisauku@gmail.com>
2018-03-08 02:14:07 +0100

    Task 2.1

--- Lab2/WordCountTensorFlow_2.py
+++ Lab2/WordCountTensorFlow_2.py
@@ -7,10 +7,12 @@ from collections import Counter
 def get_tokens():
     with open('FirstContactWithTensorFlow.txt', 'r') as tf:
         text = tf.read()
-    tokens = nltk.word_tokenize(text)
+        lowers = text.lower()
+        no_punctuation = re.sub(r'[^\w\s]','',lowers)
+        tokens = nltk.word_tokenize(no_punctuation)
     return tokens
 
 tokens = get_tokens()
 count = Counter(tokens)
 print (count.most_common(10))
-print('The total number of the words of the book is:', len(tokens))
+print('The total number of the words of the book after removing punctuation is:', len(tokens))
