aba8554592afd07c65d28850d157169b55c3d156
ferdidolot <ferdi.dolot@gmail.com>
2018-03-16 01:43:06 -0700

    Task 3.3

---
+++ Lab3/TwitterBarcelona.py
@@ -0,0 +1,75 @@
+from collections import Counter
+import json
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download("stopwords") # download the stopword corpus on our computer
+import string
+
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (10,10)
+import matplotlib.pyplot as plt
+print "backend: ", plt.get_backend()
+plt.switch_backend('agg')
+print "backend: ", plt.get_backend()
+
+punctuation = list(string.punctuation)
+stop = stopwords.words('english') + punctuation + ['rt', 'via', 'RT']
+
+# Capturing emotions, hashtag, URLs
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
+
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
+
+fname = 'Lab3.CaseStudy.json'
+with open(fname, 'r') as f:
+    count_all = Counter()
+    for line in f:
+        tweet = json.loads(line)
+        # Create a list with all the terms
+        terms_hash = [term for term in preprocess(tweet['text']) if term.startswith('#') and term not in stop]
+        count_all.update(terms_hash)
+# Print the first 10 most frequent words
+print(count_all.most_common(15))
+
+sorted_x, sorted_y = zip(*count_all.most_common(15))
+print(sorted_x, sorted_y)
+
+plt.bar(range(len(sorted_x)), sorted_y, width=0.75, align='center')
+plt.xticks(range(len(sorted_x)), sorted_x, rotation=60)
+plt.axis('tight')
+
+plt.show()                  # show it on IDE
+
+plt.savefig('CaseStudy.png')     # save it on a file
