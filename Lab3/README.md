91deaaee372af1828b4ff0c4e878e6ccd6b6c0fe
raisauku <raisauku@gmail.com>
2018-03-18 02:57:00 +0100

    Task 3.2 and 3.3

---
+++ Lab3/README.md
@@ -0,0 +1,28 @@
+## Task 3.1: Real-time tweets API of Twitter ##
+
+The dataset that we have generated contains 2316 tweets in JSON format, matching the filter criteria 'ArtificialIntelligence'.
+
+![alt text](https://github.com/ferdidolot/CLOUD-COMPUTING-CLASS-2018/blob/master/Lab3/Lab3.1_Output1.png)
+
+The last tweet of this file is displayed below: (JSON format)
+
+![alt text](https://github.com/ferdidolot/CLOUD-COMPUTING-CLASS-2018/blob/master/Lab3/Lab3.1_Output2.png)
+
+
+## Task 3.2: Analyzing tweets - Counting terms ##
+
+The following screenshot shows the list of:
+
+* Top ten most frequent tokens
+* Top ten most frequent hashtags (by default the empty hashtag (# ) was listed as one of the most frequent hashtag. We have removed it using: ` terms_hash = [term for term in preprocess(tweet['text'])
+                      if term.startswith('#') and len(term) > 1]`. This can also be solved by adding 1 `and term not in stop`)
+* Top ten most frequent terms, skipping mentions and hashtags.
+
+![alt text](https://github.com/ferdidolot/CLOUD-COMPUTING-CLASS-2018/blob/master/Lab3/Lab3.2_Output2.png)
+
+## Task 3.3: Case study ##
+
+The plot for the Case Study is:
+
+
+![alt text](https://github.com/ferdidolot/CLOUD-COMPUTING-CLASS-2018/blob/master/Lab3/CaseStudy.png)
--- Lab3/TwitterAnalyzer.py
+++ Lab3/TwitterAnalyzer.py
@@ -1,19 +1,23 @@
-import json
 import re
-import operator
 import json
+import operator
 from collections import Counter
 import nltk
-from nltk.corpus import stopwords
-nltk.download("stopwords") # download the stopword corpus on our computer
 import string
+from nltk.corpus import stopwords
+
+
+#Capturing emotions, hashtag, URLs
 
 emoticons_str = r"""
     (?:
         [:=;] # Eyes
         [oO\-]? # Nose (optional)
         [D\)\]\(\]/\\OpP] # Mouth
+        
+        
     )"""
+
 
 regex_str = [
     emoticons_str,
@@ -31,6 +35,12 @@ regex_str = [
 tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
 emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 
+
+#emoji_pattern = re.compile(r'[\U00010000-\U0010ffff]', flags=re.UNICODE)
+
+
+
+
 def tokenize(s):
     return tokens_re.findall(s)
 
@@ -40,47 +50,49 @@ def preprocess(s, lowercase=False):
         tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
     return tokens
 
+#Handling puncuation marks and terms like RT (re-tweets) and via
 
 punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + ['rt', 'via', 'RT']
 
 
-#with open('ArtificialIntelligenceTweets.json', 'r') as f:
-  #  for line in f:
-      #  tweet = json.loads(line)
-      #  tokens = preprocess(tweet['text'])
-      #  print(tokens)
-
-
+#Generate list of most frequent token, hashtag, terms skiping mentions and hashtag
 
 fname = 'ArtificialIntelligenceTweets.json'
 with open(fname, 'r') as f:
-    count_all = Counter()
+    count_stop = Counter()
     count_hashtag=Counter()
     count_only = Counter()
 
     for line in f:
         tweet = json.loads(line)
-        # Create a list with all the terms
+
+        # Create a list with all the tokens
         terms_stop = [term for term in preprocess(tweet['text']) if term not in stop]
-        count_all.update(terms_stop)
+        count_stop.update(terms_stop)
+
         # Create a list with hashtags
         terms_hash = [term for term in preprocess(tweet['text'])
-                      if term.startswith('#')]
+                      if term.startswith('#') and len(term) > 1]
         count_hashtag.update(terms_hash)
+
         #Create a list of terms onlyt skipping hashtags and mentions
         terms_only = [term for term in preprocess(tweet['text'])
                       if term not in stop and
                       not term.startswith(('#', '@'))]
         count_only.update(terms_only)
-    print ('Print the 10 top tokens')
-    for word, index in count_all.most_common(10):
-        print ('%s : %s' % (word, index))
-    print ('Print the 10 top hashtags')
-    for word, index in count_hashtag.most_common(10):
-        print ('%s : %s' % (word, index))
-    print ('Print the 10 top terms skipping mentions and hashtag')
+
+    print ('Print the 10 top tokens:')
     for word, index in count_only.most_common(10):
         print ('%s : %s' % (word, index))
+    print('\n')
+    print ('Print the 10 top hashtags:')
+    for word, index in count_hashtag.most_common(10):
+        print ('%s : %s' % (word, index))
+    print('\n')
+    print ('Print the 10 top terms skipping mentions and hashtag:')
+    for word, index in count_only.most_common(10):
+        print ('%s : %s' % (word, index))
+    print('\n')
 
 
